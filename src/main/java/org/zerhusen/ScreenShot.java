d9c82dc32fb8b98a15922eb83422f359fb02e0e2
arditsarja <arditsarja@gmail.com>
2018-05-03 21:31:00 +0200

    second comitdsfdsfds

--- src/main/java/org/zerhusen/ScreenShot.java
+++ src/main/java/org/zerhusen/ScreenShot.java
@@ -1,21 +1,41 @@
 package org.zerhusen;
 
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 public class ScreenShot {
-    private String imagepath ="";
+    private String imagepath = "";
+    private String imageInString = "";
     private BufferedImage image;
-    public ScreenShot() {
+    private byte[] byteArray;
+    private File file;
 
-        imagepath = "C:\\Users\\User\\Desktop\\" + new Date().getTime() + "screenshot.png";
+    public ScreenShot() {
+        System.setProperty("java.awt.headless", "false");
+        imagepath = "C:\\Users\\User\\Desktop\\" + new Date().getTime() + "serverscreenshot.png";
+        file = new File(imagepath);
+
         try {
             image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            ImageIO.write(image, "png", new File(imagepath));
+            ImageIO.write(image, "png", file);
+            BufferedImage bufferedImage = ImageIO.read(file);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage,"jpg",baos);
+            baos.flush();
+            byteArray=baos.toByteArray();
+            baos.close();
         } catch (HeadlessException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -26,6 +46,7 @@ public class ScreenShot {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
     }
 
     public String getImagepath() {
@@ -34,5 +55,17 @@ public class ScreenShot {
 
     public BufferedImage getImage() {
         return image;
+    }
+
+    public String getImageInString() {
+        return imageInString;
+    }
+
+    public byte[] getByteArray() {
+        return byteArray;
+    }
+
+    public File getFile() {
+        return file;
     }
 }
--- src/main/java/org/zerhusen/security/controller/StudentController.java
+++ src/main/java/org/zerhusen/security/controller/StudentController.java
@@ -14,12 +14,7 @@ import org.zerhusen.ScreenShot;
 import org.zerhusen.TypeOfServer;
 import org.zerhusen.security.repository.UserControoller;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 
 
 @CrossOrigin(origins = {"*"}, maxAge = 3600)
@@ -47,61 +42,34 @@ public class StudentController {
             }
         }
 
-        return new ResponseEntity<>("locked", HttpStatus.OK);
-    }// -------------------Retrieve All Students---------------------------------------------
+        return new ResponseEntity<>(new ScreenShot().getByteArray(), HttpStatus.OK);
+    }
+    // -------------------Retrieve All Students---------------------------------------------
 
-    @RequestMapping(value = "/sleepu", method = RequestMethod.GET)
+    @RequestMapping(value = "/sleep", method = RequestMethod.GET)
     public ResponseEntity<?> spleep() {
-        String imagesend;
         if (TypeOfServer.isWindows()) {
             try {
-                imagesend = new ScreenShot().getImage().toString();
                 Runtime.getRuntime().exec("Rundll32.exe powrprof.dll,SetSuspendState Sleep");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-        return new ResponseEntity<>("locked", HttpStatus.OK);
+        return new ResponseEntity<>(new ScreenShot().getByteArray(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/sleep", method = RequestMethod.GET)
-    public ResponseEntity<?> spleepsad() {
-//        String imagesend = "";
-//       ScreenShot dsfsdf = new ScreenShot();
-//
-//        try {
-//            imagesend = new ScreenShot().getImage().toString();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        BufferedImage image;
-        try {
-            image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            ImageIO.write(image, "png", new File("C:\\Users\\User\\Desktop\\" + new Date().getTime() + "screenshot.png"));
-        } catch (HeadlessException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (AWTException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return new ResponseEntity<>("", HttpStatus.OK);
+    @RequestMapping(value = "/screenshot", method = RequestMethod.GET)
+    public ResponseEntity<?> screenshot() {
+        return  new ResponseEntity<>(new ScreenShot().getByteArray(), HttpStatus.OK);
+//        ScreenShot s = new ScreenShot();
+//        return  new ResponseEntity<>("cfsdfds", HttpStatus.OK);
     }
 
     @RequestMapping(value = "/shutdown", method = RequestMethod.GET)
     public ResponseEntity<?> shutdown() {
-
         String shutdownCommand;
-
         if (TypeOfServer.isLinux() || TypeOfServer.isMac()) {
             shutdownCommand = "shutdown -h now";
-
         } else if (TypeOfServer.isWindows()) {
             shutdownCommand = "shutdown.exe -s -t 0";
         } else {
@@ -112,7 +80,7 @@ public class StudentController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ResponseEntity<>("Shuting Down", HttpStatus.OK);
+        return new ResponseEntity<>(new ScreenShot().getByteArray(), HttpStatus.OK);
     }
 
 
